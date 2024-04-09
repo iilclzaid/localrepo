7bee46e3c63a7dc15b39512954ec16bd0e27a741
iilclzaid <iiclzaid@gmail.com>
2024-04-09 10:58:59 +0530

    New File of Python Added

---
+++ sumOfNaturalNumbers.py
@@ -0,0 +1,33 @@
+# 1.. Sum of N Natural Numbers
+# num = int(input("Enter Value of Num"))
+# sum=0
+# for i in range(1,num+1):
+#     sum = sum+i
+# print(sum)
+
+# 2.. Factorial of N Numbers
+# num1=int(input("Enter Value of Num1"))
+# fact=1
+# for i in range(1,num1+1):
+#     fact = fact*i
+# print(fact)
+
+#  3.. Spy Number  (132,123)
+# myNum = int(input("Enter Your Number : "))
+# temp=myNum
+# rem=0
+# mySum=0
+# myMul = 1
+# while(temp > 0):
+#      rem= temp %10
+#      mySum = mySum + rem
+#      myMul = myMul * rem
+#      temp = temp//10
+
+# if(myMul == mySum):
+#     print("Spy Number")
+# else:
+#     print("Not a Spy Number")  
+
+
+# 4.. Magic Number   
